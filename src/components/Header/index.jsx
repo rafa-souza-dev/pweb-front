2a10d800997a48569ba54258e7665ace6aa4d5a2
rafael-jose <rafaeljosegomes25@hotmail.com>
2022-05-31 22:28:55 -0300

    melhorado estilo do header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,27 +6,57 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import Vector from '../../assets/vector.png';
+import { createTheme, ThemeProvider } from '@mui/material';
+import { Link, useNavigate } from 'react-router-dom';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#000',
+    },
+  },
+});
 
 export default function ButtonAppBar() {
+  const [isLogged, setLogged] = React.useState(true)
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <ThemeProvider theme={theme}>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static" sx={{ paddingInlineStart: 4, paddingInlineEnd: 4, }}>
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 1 }}
+            >
+              <img className="vector" src={Vector} alt="person" />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Controle seu peso
+            </Typography>
+            <Link to="/"
+              style={{ textDecoration: 'none', color: "#fff" }}
+            >
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                DashBoard
+              </Typography>
+            </Link>
+            {isLogged &&
+              <Link to="/perfil"
+                style={{ textDecoration: 'none', color: "#fff" }}
+              >
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1, marginLeft: 2 }}>
+                  Atualizar dados
+                </Typography>
+              </Link>
+            }
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </ThemeProvider>
   );
 }
