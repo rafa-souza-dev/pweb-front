9bf806a8926f0978752e7df06b2a46294c017a50
rafael-jose <rafaeljosegomes25@hotmail.com>
2022-06-07 23:43:03 -0300

    funcao para atualizar dados no perfil

--- src/screens/Perfil/index.jsx
+++ src/screens/Perfil/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import Logo from '../../assets/vector.png';
 import Person from '../../assets/person.png';
 import Header from '../../components/Header';
@@ -9,6 +9,8 @@ import './index.css';
 import { Button } from '@mui/material';
 import { Link, useNavigate } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material';
+import history from '../../history';
+import { Context } from '../../context/AuthContext';
 
 const theme = createTheme({
   palette: {
@@ -19,6 +21,37 @@ const theme = createTheme({
 });
 
 function Cadastro() {
+  const { handleLogin } = useContext(Context);
+
+  const [valueNome, setValueNome] = useState(localStorage.getItem("nome"));
+  const [valueEmail, setValueEmail] = useState(localStorage.getItem("email"));
+  const [valueAltura, setValueAltura] = useState(localStorage.getItem("altura"));
+  const [valueGenero, setValueGenero] = useState(localStorage.getItem("genero"));
+  const [valuePesoInicial, setValuePesoInicial] = useState(localStorage.getItem("pesoInicial"));
+  const [valuePesoFinal, setValuePesoFinal] = useState(localStorage.getItem("pesoFinal"));
+  const [valueDataFinal, setValueDataFinal] = useState(localStorage.getItem("dataFinal"));
+
+  const atualizar = () => {
+    fetch(`http://127.0.0.1:8081/api/usuario/${localStorage.getItem("id")}`, {
+      method: "PUT",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        nome: valueNome,
+        email: valueEmail,
+        altura: valueAltura,
+        genero: valueGenero,
+        pesoInicial: valuePesoInicial,
+        pesoFinal: valuePesoFinal,
+        dataFinal: valueDataFinal,
+      })
+    });
+    handleLogin();
+    history.push("/perfil");
+    history.go();
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <div>
@@ -51,17 +84,33 @@ function Cadastro() {
                     autoComplete="off"
                   >
                     <TextField id="outlined-basic" label="Nome" variant="outlined" fullWidth
-                      defaultValue="Rafael Souza"
+                      defaultValue={localStorage.getItem("nome")}
+                      onChange={event => {
+                        const { value } = event.target;
+                        setValueNome(value);
+                      }}
                     />
                     <TextField id="outlined-basic" label="Email" variant="outlined" fullWidth
-                      defaultValue="rafael@exemplo.com"
+                      defaultValue={localStorage.getItem("email")}
+                      onChange={event => {
+                        const { value } = event.target;
+                        setValueEmail(value);
+                      }}
                     />
                     <TextField id="outlined-basic" label="Altura (Cm)" variant="outlined"
-                      defaultValue={175}
+                      defaultValue={localStorage.getItem("altura")}
+                      onChange={event => {
+                        const { value } = event.target;
+                        setValueAltura(value);
+                      }}
                     />
                     <TextField id="outlined-basic" label="Gênero/Sexo" variant="outlined"
                       sx={{ marginLeft: 1 }}
-                      defaultValue="Masculino"
+                      defaultValue={localStorage.getItem("genero")}
+                      onChange={event => {
+                        const { value } = event.target;
+                        setValueGenero(value);
+                      }}
                     />
                   </Box>
                   <h3>Metas</h3>
@@ -74,17 +123,29 @@ function Cadastro() {
                     autoComplete="off"
                   >
                     <TextField id="outlined-basic" label="Peso inicial(atual)" variant="outlined"
-                      defaultValue={100}
+                      defaultValue={localStorage.getItem("pesoInicial")}
+                      onChange={event => {
+                        const { value } = event.target;
+                        setValuePesoInicial(value);
+                      }}
                     />
                     <TextField id="outlined-basic" label="Peso Desejado" variant="outlined"
                       sx={{ marginLeft: 1 }}
-                      defaultValue={80}
+                      defaultValue={localStorage.getItem("pesoFinal")}
+                      onChange={event => {
+                        const { value } = event.target;
+                        setValuePesoFinal(value);
+                      }}
                     />
                     <TextField id="outlined-basic" label="Data para alcançar" variant="outlined"
-                      defaultValue="11/11/1111"
+                      defaultValue={localStorage.getItem("dataFinal")}
+                      onChange={event => {
+                        const { value } = event.target;
+                        setValueDataFinal(value);
+                      }}
                     />
                   </Box>
-                  <Button variant='contained'>
+                  <Button variant='contained' onClick={() => atualizar()}>
                     Atualizar
                   </Button>
                 </div>
