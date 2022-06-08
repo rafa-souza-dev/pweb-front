e94a1ccba19bd1cda22abeb469d59b5f99657df6
rafael-jose <rafaeljosegomes25@hotmail.com>
2022-06-07 22:14:43 -0300

    rotas privadas para usuario

--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,45 @@
-import React, { useContext } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Cadastro from './screens/Cadastro';
 import Login from './screens/Login';
 import Perfil from './screens/Perfil';
+
+const PrivateRoute = ({ children, redirectTo }) => {
+    const isAuthenticated = localStorage.getItem("id") !== null;
+    return isAuthenticated ? children : <Navigate to={redirectTo} />
+}
+
+const PrivateRouteLogged = ({ children, redirectTo }) => {
+    const isAuthenticated = localStorage.getItem("id") !== null;
+    return isAuthenticated ? <Navigate to={redirectTo} /> : children
+}
 
 export default function CustomRoutes() {
     return (
         <Routes>
             <Route 
                 path="/cadastro" 
-                element={<Cadastro />}
+                element={
+                    <PrivateRouteLogged redirectTo={"/"}>
+                        <Cadastro />
+                    </PrivateRouteLogged>
+                }
             />
             <Route 
                 path="/" 
-                element={<Login />}
+                element={
+                    <PrivateRouteLogged redirectTo={"/"}>
+                        <Login />
+                    </PrivateRouteLogged>
+                }
             />
             <Route 
                 path="/perfil" 
-                element={<Perfil />}
+                element={
+                    <PrivateRoute redirectTo={"/"}>
+                        <Perfil />
+                    </PrivateRoute>
+                }
             />
         </Routes>
     );
