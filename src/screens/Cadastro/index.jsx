ca4f44f15df9c93dd77e8ca88f8d87176e552ea0
rafael-jose <rafaeljosegomes25@hotmail.com>
2022-06-07 23:26:51 -0300

    funcao para criar conta

--- src/screens/Cadastro/index.jsx
+++ src/screens/Cadastro/index.jsx
@@ -9,6 +9,7 @@ import './index.css';
 import { Button } from '@mui/material';
 import { Link, useNavigate } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material';
+import history from '../../history';
 
 const theme = createTheme({
   palette: {
@@ -25,7 +26,27 @@ function Cadastro() {
   const [valueGenero, setValueGenero] = useState("");
   const [valuePesoInicial, setValuePesoInicial] = useState("");
   const [valuePesoFinal, setValuePesoFinal] = useState("");
-  const [valueData, setValueData] = useState("");
+  const [valueDataFinal, setValueDataFinal] = useState("");
+
+  const cadastrar = () => {
+    fetch(`http://127.0.0.1:8081/api/usuario`, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        nome: valueNome,
+        email: valueEmail,
+        altura: valueAltura,
+        genero: valueGenero,
+        pesoInicial: valuePesoInicial,
+        pesoFinal: valuePesoFinal,
+        dataFinal: valueDataFinal,
+      })
+    });
+    history.push("/");
+    history.go();
+  }
 
   return (
     <ThemeProvider theme={theme}>
@@ -56,25 +77,25 @@ function Cadastro() {
                     noValidate
                     autoComplete="off"
                   >
-                    <TextField id="outlined-basic" label="Nome" variant="outlined" fullWidth 
-                    defaultValue={valueNome}
-                    onChange={event => {
-                      const { value } = event.target;
-                      setValueNome(value);
-                    }}/>
-                    <TextField id="outlined-basic" label="Email" variant="outlined" fullWidth 
-                    defaultValue={valueEmail}
-                    onChange={event => {
-                      const { value } = event.target;
-                      setValueEmail(value);
-                    }}/>
-                    <TextField id="outlined-basic" label="Altura (Cm)" variant="outlined" 
-                    defaultValue={valueAltura}
-                    onChange={event => {
-                      const { value } = event.target;
-                      setValueAltura(value);
-                    }}/>
-                    <TextField id="outlined-basic" label="Gênero/Sexo" variant="outlined"
+                    <TextField id="outlined-basic" label="Nome" variant="outlined" fullWidth
+                      defaultValue={valueNome}
+                      onChange={event => {
+                        const { value } = event.target;
+                        setValueNome(value);
+                      }} />
+                    <TextField id="outlined-basic" label="Email" variant="outlined" fullWidth
+                      defaultValue={valueEmail}
+                      onChange={event => {
+                        const { value } = event.target;
+                        setValueEmail(value);
+                      }} />
+                    <TextField id="outlined-basic" label="Altura (Cm)" variant="outlined"
+                      defaultValue={valueAltura}
+                      onChange={event => {
+                        const { value } = event.target;
+                        setValueAltura(value);
+                      }} />
+                    <TextField id="outlined-basic" label="Gênero/Sexo (MASCULINO/FEMININO)" variant="outlined"
                       sx={{ marginLeft: 1 }}
                       defaultValue={valueGenero}
                       onChange={event => {
@@ -92,12 +113,12 @@ function Cadastro() {
                     noValidate
                     autoComplete="off"
                   >
-                    <TextField id="outlined-basic" label="Peso inicial(atual)" variant="outlined" 
-                    defaultValue={valuePesoInicial}
-                    onChange={event => {
-                      const { value } = event.target;
-                      setValuePesoInicial(value);
-                    }}/>
+                    <TextField id="outlined-basic" label="Peso inicial(atual)" variant="outlined"
+                      defaultValue={valuePesoInicial}
+                      onChange={event => {
+                        const { value } = event.target;
+                        setValuePesoInicial(value);
+                      }} />
                     <TextField id="outlined-basic" label="Peso Desejado" variant="outlined"
                       sx={{ marginLeft: 1 }}
                       defaultValue={valuePesoFinal}
@@ -106,9 +127,14 @@ function Cadastro() {
                         setValuePesoFinal(value);
                       }}
                     />
-                    <TextField id="outlined-basic" label="Data para alcançar" variant="outlined" />
+                    <TextField id="outlined-basic" label="Data para alcançar" variant="outlined" 
+                    defaultValue={valueDataFinal}
+                    onChange={event => {
+                      const { value } = event.target;
+                      setValueDataFinal(value);
+                    }} />
                   </Box>
-                  <Button variant='contained'>
+                  <Button variant='contained' onClick={() => cadastrar()} >
                     Finalizar
                   </Button>
                   <Link to="/"
